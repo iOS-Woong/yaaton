c4cd0d30e8092d1def48d1fe1e3a591312a01035
woong <workplayhard1@naver.com>
2022-09-14 09:12:35 +0900

    first commit

---
+++ yaaton01-Sooji,woong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yaaton01-Sooji,woong
+//
+//  Created by 서현웅 on 2022/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
